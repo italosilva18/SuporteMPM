28d4d5c622c05309284bec4dd1e1110f4c431fb5
italosilva18 <italosilva14@hotmail.com>
2022-09-29 08:20:46 -0300

    Novo

--- SuporteMPM/urls.py
+++ SuporteMPM/urls.py
@@ -1,4 +1,4 @@
-"""SuportMPM URL Configuration
+"""SuporteMPM URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('form/', form),
 ]
--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from app.models import Cadatro_Lojas
-
-# Create the form class.
-class Cadatro_LojasForm(ModelForm):
-    class Meta:
-        model = Cadatro_Lojas
-        fields = ['RAZAO_SOCIAL', 'CNPJ', 'NOME_FANTASIA', 'RESPONSAVEL', 'VENDEDOR', 'AUTOMACAO', 'ANALISTA', 'DATA_ATIVACAO', 'STATUS']
