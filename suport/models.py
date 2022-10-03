f7a2af385b705cccf341db9b0c6d1386c047831c
italosilva18 <italosilva14@hotmail.com>
2022-10-02 23:00:36 -0300

    Criação de models

--- suport/models.py
+++ suport/models.py
@@ -1,3 +1,21 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class suporte(models.Model):
+    razao_social = models.CharField(max_length=100)   
+    nome_fantasia= models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=20)
+    automacao = models.CharField(max_length=50)
+    cod_id = models.CharField(max_length=100)
+    nome_contato = models.CharField(max_length=100)
+    tel_contato = models.CharField(max_length=100)
+    vendedor = models.CharField(max_length=100)
+    analista = models.CharField(max_length=100)
+    data_instal = models.DateTimeField()
+    install = models.BooleanField()
+    status = models.CharField(max_length=100)
+    obs = models.TextField()
+    slug = models.SlugField()
